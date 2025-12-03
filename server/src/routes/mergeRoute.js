c63f781988b47beab280e55aacebb9b225b9aba1
ejmichael <ethanj.michael03@gmail.com>
2025-12-04 01:08:55 +0200

    Added merge functionality

--- server/src/routes/mergeRoute.js
+++ server/src/routes/mergeRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const mergePDFController = require("../controllers/mergePdfController");
+const mergePDFController = require("../controllers/mergePDFController");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
